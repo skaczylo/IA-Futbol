2c6228c54d8c49ef30197ca8f6a1932d72f705aa
Adrian <adrisawicka@gmail.com>
2025-08-11 10:00:36 +0200

    Version proyecto inicial

---
+++ dataset_engineering/active_learning/active_learning.py
@@ -0,0 +1,215 @@
+from dotenv import load_dotenv
+import os
+from ultralytics import YOLO
+from label_studio_sdk import LabelStudio
+import requests
+from PIL import Image
+import requests
+from tqdm import tqdm
+from io import BytesIO
+import zipfile
+import io
+import shutil
+
+load_dotenv()
+
+class LS:
+
+    def __init__(self,project_name,model,model_name):
+        """
+        Conexion al proyecto
+        """
+        self.model = model
+        self.model_name = model_name
+        #Datos de acceso
+        
+        self.API_KEY = os.getenv("API_KEY")
+        self.LABEL_STUDIO_URL = os.getenv("LABEL_STUDIO_URL")
+        self.URL_REFRESH = os.getenv("URL_REFRESH") #Url para refrescar token de acceso
+        self.BASE_URL = os.getenv("BASE_URL") #Base url de label studio: puerto 8080
+        self.DOWNLOAD_PATH = os.getenv("DOWNLOAD_PATH") #Ruta donde se descargan las imagenes completadas
+        self.IMG_PATH = os.getenv("IMG_PATH") #Ruta a las imagenes generadas
+        self.FT_PATH = os.getenv("FT_PATH") #Ruta a la carpeta Fine tuning
+
+        response = requests.post(self.URL_REFRESH,json={"refresh": self.API_KEY})
+        self.ACCESS_TOKEN = response.json().get("access")
+
+        #Conexion al proyecto
+        #Conectamos con label studio
+        self.client = LabelStudio(api_key=self.API_KEY)
+        projects_info = self.client.projects.list()
+        self.project = next((p for p in projects_info if p.title == project_name),None)
+        self.project = self.client.projects.get(self.project.id)
+
+        #sincronizamos datos
+        storages = self.client.import_storage.local.list(project=self.project.id)
+        for strg in storages:
+            self.client.import_storage.local.sync(strg.id)
+    
+    def refresh_token(self):
+        response = requests.post(self.URL_REFRESH,json={"refresh": self.API_KEY})
+        self.ACCESS_TOKEN = response.json().get("access")
+
+    def predict_img(self,image,img_width,img_height):
+        """
+        Dada una imagen, el modelo del proyecto predice la imagen
+        """
+
+        results = self.model(image,verbose=False)
+        for result in results:
+
+            boxes = result.boxes.cpu().numpy()
+            prediction = {'result': [], 'score': 0.0, 'model_version': self.model_name}
+            scores = []
+            for box, class_id, score in zip(boxes.xywh, boxes.cls, boxes.conf):
+                x, y, w, h = box
+                prediction['result'].append({
+                    'from_name': 'label',
+                    'to_name': 'img',
+                    'original_width': int(img_width),
+                    'original_height': int(img_height),
+                    'image_rotation': 0,
+                    'value': {
+                        'rotation': 0,
+                        'rectanglelabels': [result.names[class_id]],
+                        'width': w / img_width * 100,
+                        'height': h / img_height * 100,
+                        'x': (x - 0.5 * w) / img_width * 100,
+                        'y': (y - 0.5 * h) / img_height * 100
+                    },
+                    'score': float(score),
+                    'type': 'rectanglelabels',
+                })
+                scores.append(float(score))
+            prediction['score'] = min(scores) if scores else 0.0
+        
+        return prediction
+    
+
+    def predict_all_tasks(self):
+
+        tasks = self.client.tasks.list(project=self.project.id)
+        for i, task in enumerate(tqdm(tasks)):
+        
+            url = f'{self.BASE_URL}{task.data['image']}'
+           
+            self.refresh_token()
+            request = requests.get(url, headers={'Authorization': f'Bearer {self.ACCESS_TOKEN}'}, stream=True)
+            image = Image.open(request.raw)
+               
+            w,h = image.size
+            predictions = self.predict_img(image,w,h)
+            self.client.predictions.create(task=task.id, result=predictions['result'], score=predictions['score'], model_version=predictions['model_version'])
+
+    def predict_new_tasks(self):
+        tasks = self.client.tasks.list(project = self.project.id)
+
+        for i, task in enumerate(tqdm(tasks)):
+            dic = task.dict()
+
+            if dic["total_predictions"] == 0:
+                url = f'{self.BASE_URL}{task.data['image']}'
+                self.refresh_token()
+                request = requests.get(url, headers={'Authorization': f'Bearer {self.ACCESS_TOKEN}'}, stream=True)
+                image = Image.open(request.raw)
+                w,h = image.size
+                predictions = self.predict_img(image,w,h)
+                self.client.predictions.create(task=task.id, result=predictions['result'], score=predictions['score'], model_version=predictions['model_version'])
+
+
+
+    def export_completed_tasks(self):
+        """
+        Exporta las etiquetas de las imagenes revisadas a la carpeta destino junto a las imagenes
+        path = destino donde se descargaran las etiquetas en formato YOLO
+        """
+
+        #Creamos una nueva snapshot
+        url =f'{self.BASE_URL}/api/projects/{self.project.id}/exports/'
+        self.refresh_token()
+        snapshot_rq = requests.post(url,headers={'Authorization': f'Bearer {self.ACCESS_TOKEN}'},json = {"task_filter_options": {"finished": "only"}})
+
+        if snapshot_rq.status_code == 201:
+            data = snapshot_rq.json()
+            export_pk = data["id"]
+        else:
+            print(snapshot_rq.text)
+
+        #Descargamos la snapshot en formato YOLO
+        url = f'{self.BASE_URL}/api/projects/{self.project.id}/exports/{export_pk}/download?exportType=YOLO'
+        self.refresh_token()
+        download_snapshot_rq = requests.get(url,headers={'Authorization': f'Bearer {self.ACCESS_TOKEN}'})
+
+        if download_snapshot_rq.status_code == 200:
+            with zipfile.ZipFile(io.BytesIO(download_snapshot_rq.content)) as z:
+            # Extraemos todo en la carpeta actual (o especifica otra ruta)
+                z.extractall(path=self.DOWNLOAD_PATH)
+                print("Donwload ok")
+
+            #Copiamos las imagenes y la etiquetas a fine_tuning
+            labels_path = os.path.join(self.DOWNLOAD_PATH,"labels")
+            imgs_path = os.path.join(self.IMG_PATH)
+            labels = os.listdir(labels_path)
+
+            for label in labels:
+                name,extension = os.path.splitext(label)
+
+                #Rutas originales
+                img_path = os.path.join(imgs_path,name+".jpg")
+                label_path = os.path.join(labels_path,label)
+
+
+                #Mover a rutas destinos
+                try:
+                    shutil.copy(img_path,os.path.join(self.FT_PATH,"train","images",name+".jpg"))
+                    shutil.copy(label_path,os.path.join(self.FT_PATH,"train","labels",label))
+                    os.remove(label_path) 
+                except:
+                    print(f"Error during copy: {name}.jpg")
+
+
+
+
+
+    def increase_dataset(self):
+        """
+        Mueve las nuevas imagenes etiquetas y almacendas en fine-tuning, al dataset principal de entrenamiento del modelo
+        """
+        
+    def delete_completed_tasks(self):
+        """
+        Elimina todos los tasks completados
+        """
+
+        tasks = self.client.tasks.list(project = self.project.id)
+    
+        for task in tasks:
+        
+            dic = task.dict()
+            if dic["completed_at"] != None: #esta completada
+                self.client.tasks.delete(id = dic["id"])#eliminar task
+                os.remove(f'/{task.data["image"].split("=")[1]}') #eliminar del disco
+                
+
+        
+
+    def delete_task(self,t_id):
+        task = self.client.tasks.get(id = t_id)
+        self.client.tasks.delete(id=t_id)
+        os.remove(f'/{task.data["image"].split("=")[1]}')
+
+    def delete_all_tasks(self):
+        """
+        Elimina todas las imagenes a marcar
+        """
+        self.client.tasks.delete_all_tasks(self.project.id)
+
+
+
+        
+
+        
+        
+
+
+    
---
+++ dataset_engineering/data_generator/extract_frames.py
@@ -0,0 +1,57 @@
+"""
+Extrae frames de los videos descargados y los almacena en el dataset para su posterior etiquetado
+
+"""
+import os
+import cv2 as cv
+import numpy as np
+import time
+import uuid
+
+RESULT_PATH ="dataset_engineering/data_generator/generated_images"
+VIDEOS_PATH = "dataset_engineering/data_generator/videos"
+
+
+def extract_frames(video_path,output_path,percentage = 0.2,delete=True):
+    """
+    Extrae del video dado un 20% de los frames y los almacena en la ruta indicada por output_path.
+    Ademas, por defecto, elimina el video una vez extraida las imagenes
+    """
+
+    video = cv.VideoCapture(video_path)
+    fps = video.get(cv.CAP_PROP_FPS) #Obtener fps
+    total_frames = int(video.get(cv.CAP_PROP_FRAME_COUNT))
+
+    total_seconds = total_frames/fps
+    number_seconds = int(percentage *(total_seconds-20)) #numero de frames que queremos obtener: un 20% por defecto
+
+    selected_seconds = np.linspace(10, total_seconds-10, number_seconds,dtype=int)
+
+    for second in selected_seconds:
+        video.set(cv.CAP_PROP_POS_FRAMES, second*fps)
+        ret, frame = video.read()
+
+        if not ret or frame is None:
+            print(f"Failed to read frame at {second}s in {video_path}")
+            continue
+
+        frame_id = f"{time.strftime("%Y%m%d_%H%M%S")}_{uuid.uuid4().hex[:6] }" # formato tipo 20250710_155203;parte aleatoria para evitar duplicados
+        cv.imwrite(os.path.join(output_path,f"{frame_id}.jpg"),frame)
+
+
+        
+    video.release()
+    if delete:
+        os.remove(video_path)
+
+
+
+
+
+    
+
+
+
+
+
+
