b5c4cc619c429e83c83174973e791bf5c17decb4
Adrian <adrisawicka@gmail.com>
2026-03-22 10:52:26 +0100

    Clasificacion de equipos

--- pitch_keypoints/draw_pitch.py
+++ pitch_keypoints/draw_pitch.py
@@ -1,12 +1,12 @@
 import matplotlib.pyplot as plt
 from matplotlib.patches import Arc, Circle
-from datatools.ellipse import PITCH_POINTS
 import numpy as np
 import cv2
+import torch
 
 # Asumo que tienes definido GREEN en tu entorno, por ejemplo: GREEN = '#4C9A2A'
 GREEN = '#4C9A2A'
-def get_pitch_image(keypoints= None, figsize = (8.5,4.8)):
+def draw_pitch(figsize = (8.5,4.8)):
     fig, ax = plt.subplots(figsize=figsize)
     
     # Color de fondo verde estilo césped
@@ -59,37 +59,100 @@ def get_pitch_image(keypoints= None, figsize = (8.5,4.8)):
     ax.plot([52.5, 54.5, 54.5, 52.5], [-3.66, -3.66, 3.66, 3.66], color="white", linewidth=2)
     
     # Mantener las proporciones y quitar ejes
-    ax.set_aspect('equal') # Esto asegura que no se deformen los círculos si cambias el tamaño de la figura
+    ax.set_aspect('equal') 
     ax.axis('off')
     ax.set_xlim(-54.5, 54.5)
     ax.set_ylim(-35, 35)
-    #fig.subplots_adjust(left=0, right=1, top=1, bottom=0)
-
-    #keypoints 
-
-    if keypoints is not None and len(keypoints) > 0 :
-        x_coords = keypoints[:, 0]
-        y_coords = keypoints[:, 1]
-    
-        ax.scatter(x_coords, y_coords, color="red", s=100, edgecolors="white", linewidths=1.5, zorder=5)
-
-
+   
     ax.invert_yaxis()
     fig.subplots_adjust(left=0, right=1, bottom=0, top=1, wspace=0, hspace=0)
-    fig.canvas.draw()
+    #fig.canvas.draw()
+
+
+    return fig, ax
+
+def get_pitch_image(players_keypoints = None, team_segmentation = None):
+
+    fig , ax  = draw_pitch()
     
-    # 1. Convertir el canvas a un buffer RGBA
-    rgba_buffer = fig.canvas.buffer_rgba()
+    if players_keypoints is None: #Mapa 2D vacía
+        fig.canvas.draw()
+        rgba_buffer = np.array(fig.canvas.buffer_rgba())
+        pitch_img = cv2.cvtColor(rgba_buffer, cv2.COLOR_RGBA2BGR)
+        plt.close(fig)
+        return pitch_img
 
-    # 2. Convertir a un array de numpy
-    img_array = np.asarray(rgba_buffer)
 
-    # 3. Matplotlib genera RGBA, pero OpenCV suele usar BGR. 
-    # Si vas a usar esta imagen con cv2.imshow o escribirla en un video:
-    img_bgr = cv2.cvtColor(img_array, cv2.COLOR_RGBA2BGR)
+    if team_segmentation is None: #No team segmentation
+        num_players = len(players_keypoints)
+        team_segmentation = np.zeros(num_players)
 
-    # Es MUY importante cerrar la figura para no saturar la memoria RAM en un bucle de video
+
+    for (kpt,team) in zip(players_keypoints,team_segmentation):
+
+    
+        x_coords = kpt[0]
+        y_coords = kpt[1]
+
+        if team == 0:
+            ax.scatter(x_coords, y_coords, color="red", s=100, edgecolors="white", linewidths=1.5, zorder=5)
+        else:
+            ax.scatter(x_coords, y_coords, color="blue", s=100, edgecolors="white", linewidths=1.5, zorder=5)
+
+
+    fig.canvas.draw()
+    rgba_buffer = np.array(fig.canvas.buffer_rgba())
+    pitch_img = cv2.cvtColor(rgba_buffer, cv2.COLOR_RGBA2BGR)
     plt.close(fig)
+    
+    return pitch_img
 
-    return img_bgr
+def overlay_pitch_minimap(frame, pitch_img, alpha=0.7, scale_factor=0.35):
+   
+    #Ajustar el tamaño del minimapa proporcionalmente al frame
+    frame_h, frame_w = frame.shape[:2]
+    map_w = int(frame_w * scale_factor)
+    aspect_ratio = pitch_img.shape[0] / pitch_img.shape[1]
+    map_h = int(map_w * aspect_ratio)
+    
+    minimap = cv2.resize(pitch_img, (map_w, map_h), interpolation=cv2.INTER_AREA)
+
+    # Abajo al Centro
+    x_offset = (frame_w - map_w) // 2
+    y_offset = frame_h - map_h - 10
+
+    #Definir la Región de Interés (ROI) en el frame original
+    roi = frame[y_offset:y_offset+map_h, x_offset:x_offset+map_w]
+
+    # 4. Mezclar las imágenes para dar transparencia
+    # Formula: frame_final = roi * (1 - alpha) + minimap * alpha
+    blended = cv2.addWeighted(roi, 1 - alpha, minimap, alpha, 0)
+
+    # 5. Insertar la mezcla de vuelta en el frame original
+    frame_copy = frame.copy()
+    frame_copy[y_offset:y_offset+map_h, x_offset:x_offset+map_w] = blended
+
+    return frame_copy
+
+
+def draw_keypoints(frame,keypoints:torch.Tensor, conf = 0.85):
+    """
+    Dibuja keypoints sobre la imagen dada
+    """
+
+    annotated_frame = frame.copy()
+
+    if len(keypoints) > 1:
+        keypoints = keypoints[0]
+
+    keypoints = keypoints.squeeze(0)
+
+    
+    for i, kpt in enumerate(keypoints):
+
+        x , y, kpt_conf = kpt
+        if kpt_conf >= conf:
+            cv2.circle(annotated_frame, (int(x), int(y)), radius=5, color=PALETA_COLORES[i], thickness=cv2.FILLED)
+    
+    return annotated_frame
     
---
+++ pitch_keypoints/homography.py
@@ -0,0 +1,41 @@
+import numpy as np
+from datatools.ellipse import INTERSECTON_TO_PITCH_POINTS
+from datatools.ellipse import PITCH_POINTS
+import cv2
+
+
+def get_homography(keypoints_detected,conf = 0.85):
+
+    """
+    Calcula, si se puede, Homografia H de pixeles a mapa 2D
+    """
+
+    H = None
+    if len(keypoints_detected) > 1:
+        keypoints_detected = keypoints_detected[0]
+
+    keypoints_detected = keypoints_detected.squeeze(0)
+
+    src_points = [] #Puntos de la imagen
+    trg_points = [] #Puntos del campo 2D
+
+    for i, kpt in enumerate(keypoints_detected):
+
+        x,y,kpt_conf = kpt
+
+        if kpt_conf >= conf:
+
+
+            kpt_cls = INTERSECTON_TO_PITCH_POINTS[i]
+            src_points.append([int(x),int(y)])
+            trg_points.append(PITCH_POINTS[kpt_cls])
+
+
+    #Formato especifico cv2
+    src_points = np.array(src_points, dtype=np.float32)
+    trg_points = np.array(trg_points, dtype=np.float32)
+
+    if len(src_points) >= 4:
+        H, status = cv2.findHomography(src_points, trg_points, cv2.RANSAC, 5.0)
+        
+    return H
